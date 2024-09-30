4894e9de0b4c12b9c248c61766381f916cf7577f
Lev <kakalashvililev@yandex.ru>
2024-09-30 20:01:11 +0300

    Homework_2: Добавлена пагинация

--- app/core/models/common.py
+++ app/core/models/common.py
@@ -11,15 +11,22 @@ class BasePaginationParams(BaseModel):
     """
 
     limit: int | None = Field(
-        Query(100, ge=1, le=100, description="Размер ограничения")
+        Query(None, ge=1, le=100, description="Размер ограничения", example=100),
     )
-    offset: int | None = Field(Query(0, ge=0, description="Размер смещения"))
+    offset: int | None = Field(
+        Query(None, ge=0, description="Размер смещения", example=0)
+    )
 
     def add_pagination(self, query: Union[str, Select]) -> Union[str, Select]:
         """
         Добавить пагинацию (ограничение/смещение) к запросу
         """
         if isinstance(query, str):
-            return f"{query} LIMIT {self.limit} OFFSET {self.offset}"
+            if self.limit is not None and self.offset is not None:
+                return f"{query} LIMIT {self.limit} OFFSET {self.offset}"
+            elif self.limit is not None and self.offset is None:
+                return f"{query} LIMIT {self.limit} OFFSET 0"
+            elif self.limit is None:
+                return query
 
         return query.offset(self.offset).limit(self.limit)
